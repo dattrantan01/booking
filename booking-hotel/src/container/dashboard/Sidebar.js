5011a9734fc98acdf60b3298018d4087caa89b35
Tran Tan Dat <dattrantan01@gmail.com>
2023-06-13 00:13:22 +0700

    feat: navigate in manange layout

--- booking-hotel/src/container/dashboard/Sidebar.js
+++ booking-hotel/src/container/dashboard/Sidebar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { sidebarLinks } from "./helpers";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./styles.scss";
+import { PATHS } from "../../utils/paths";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
   return (
     <div className="side-bar w-[300px] rounded-xl shadow-xl">
-      <div className="sidebar-logo cursor-pointer flex items-center mb-5 pt-5 px-5 font-medium gap-5">
+      <div
+        className="sidebar-logo cursor-pointer flex items-center mb-5 pt-5 px-5 font-medium gap-5 cursor-pointer"
+        onClick={() => navigate(PATHS.base)}
+      >
         <div className="h-[40px] w-[40px]">
           <img
             src="/logo.png"
